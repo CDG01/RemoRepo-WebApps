a718cbb2ea718323ecc36854bdef0e8262356280
Chiara De Guglielmo <154986750+CDG01@users.noreply.github.com>
2024-06-09 16:20:14 +0200

    try

---
+++ Interceptors-LogAndLegacyMiddeleware_webApp/src/main/java/com/Interceptors/LogAndLegacyMiddleware_webApp/LogAndLegacyMiddleware_webAppApplication.java
@@ -0,0 +1,13 @@
+package com.Interceptors.LogAndLegacyMiddleware_webApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogAndLegacyMiddleware_webAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogAndLegacyMiddleware_webAppApplication.class, args);
+	}
+
+}
